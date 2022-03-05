d278e60c735c33bd11488bc2f99b89f07d445413
weblerson <zonejr4@gmail.com>
2022-03-04 23:34:43 -0400

    devdb

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-CONNECTION = f"sqlite:///mercearia.db"
+CONNECTION = f"sqlite:///test.db"
 
 engine = create_engine(CONNECTION, echo = False)
 
